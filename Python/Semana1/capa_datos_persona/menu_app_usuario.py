cb83f1846c3e203ff6c41d8e9aaf0e338584970e
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-08-15 19:23:36 -0300

    Update menu_app_usuario.py
    uodate menu

--- Python/Semana1/capa_datos_persona/menu_app_usuario.py
+++ Python/Semana1/capa_datos_persona/menu_app_usuario.py
@@ -15,7 +15,24 @@ while opcion != 5:
         usuarios = UsuarioDAO.seleccionar()
         for usuario in usuarios:
             log.info(usuario)
-
-else:
-    log.info('Salimos del programa')
+    elif opcion == 2:
+        username_var = input('Ingrese el username: ')
+        password_var = input('Ingrese el password: ')
+        usuario = Usuario(username=username_var, password=password_var)
+        usuario_insertado = UsuarioDAO.insertar(usuario)
+        log.info(f'Usuario insertado: {usuario_insertado}')
+    elif opcion == 3:
+        id_usuario_var = int(input('Ingrese el id_usuario: '))
+        username_var = input('Ingrese el username: ')
+        password_var = input('Ingrese el password: ')
+        usuario = Usuario(id_usuario=id_usuario_var, username=username_var, password=password_var)
+        usuario_actualizado = UsuarioDAO.actualizar(usuario)
+        log.info(f'Usuario actualizado: {usuario_actualizado}')
+    elif opcion == 4:
+        id_usuario_var = int(input('Ingrese el id_usuario: '))
+        usuario = Usuario(id_usuario=id_usuario_var)
+        usuario_eliminado = UsuarioDAO.eliminar(usuario)
+        log.info(f'Usuario eliminado: {usuario_eliminado}')
+    else:
+        log.info('Salimos del programa')
     

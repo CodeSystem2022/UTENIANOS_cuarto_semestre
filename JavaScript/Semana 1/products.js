b78b37182d9b785100657f8eff8d6b3057af193c
Carolina Belén Delgado <98441984+programacioncarolina1980@users.noreply.github.com>
2023-09-08 20:26:41 -0300

    Add files via upload
    se agrega archivo de productos js

---
+++ JavaScript/Semana 1/products.js
@@ -0,0 +1,16 @@
+const productos= [ 
+    { 
+        id: 1,
+     productName: "Banana",
+     price: 480,
+     quanty: 1,
+     img: "\Client\media\banana.png" ,
+     },
+    { 
+        id: 2,
+     productName: "Leche",
+     price: 950,
+     quanty: 1,
+     img: "\Client\media\leche.png",
+     },
+ ]

bfb7deca8cc6ab91e1a731f39c626c59d6efd9ba
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-09-13 22:37:56 -0300

    Add files via upload

---
+++ Node/Semana 2/app.js
@@ -0,0 +1,16 @@
+/////////////////////////////////
+//  let nombre = 'Agus';
+//  console.log(nombre);
+
+
+//  nombre = 'Raul';
+//  console.log(nombre);
+//////////////////////////////////
+
+const nombre = 'Jesus';
+console.log(nombre);
+
+const nombre2 = "Roberto";
+console.log(nombre2);
+
+console.log('Hola Mundo desde Node');

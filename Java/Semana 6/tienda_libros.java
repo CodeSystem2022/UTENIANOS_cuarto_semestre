ab470de92ed1797f84d55617ae4147cb5cead909
Celina Leonarda Costilla <88327832+Hiddencelc@users.noreply.github.com>
2023-10-01 22:17:26 -0300

    Update tienda_libros.java
    Empezamos nuestro segundo proyecto con Spring, Tienda de Libros, hacemos las conexiones correctas y empezamos con nuestro modelo.

--- Java/Semana 6/tienda_libros.java
+++ Java/Semana 6/tienda_libros.java
@@ -1,1 +1,37 @@
+# CONEXION A MYSQL - ARCHIVO PROPERTIES
 
+spring.datasource.url=jdbc:mysql://localhost:3306/tienda_libros_db?createDatabaseIfNotExist=true
+spring.datasource.username=root
+spring.datasource.password= // contraseña de cada uno
+spring.datasource.driver-class-name= com.mysql.cj.jdbc.Driver
+
+#CREACION DE LA BASE DE DATOS EN CASO DE SER NECESARIO
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.show-sql=true
+  
+
+package Utn.tienda_libros.modelo;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@ToString
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idlibro;
+    String nombreLibro;
+    String autor;
+    Double precio;
+    Integer existencias;
+
+}

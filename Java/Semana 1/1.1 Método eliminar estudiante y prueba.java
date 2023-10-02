8d67f89de92762c75469ce1ef1ba9276238f4115
Celina Leonarda Costilla <88327832+Hiddencelc@users.noreply.github.com>
2023-10-01 21:56:16 -0300

    Update 1.1 Método eliminar estudiante y prueba.java
    último Métdo visto en Sistema Estudiante, eliminar Estudiante.

--- Java/Semana 1/1.1 Método eliminar estudiante y prueba.java
+++ Java/Semana 1/1.1 Método eliminar estudiante y prueba.java
@@ -1,1 +1,22 @@
+public boolean eliminarEstudiante(Estudiante estudiante){
+        PreparedStatement ps;
+        Connection con= getConnection();
+        String sql= "DELETE FROM  estudiantes2022 WHERE idestudiantes2022=?";
+        try{
+            ps= con.prepareStatement(sql);
+            ps.setInt(1,estudiante.getIdEstudiante());
+            ps.execute();
+            return true;
 
+        } catch (Exception e){
+            System.out.println("Error al eliminar estudiante:" + e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            }catch (Exception e){
+                System.out.println("Error al cerrar la conexión: "+ e.getMessage());
+            }
+        }
+        return false;
+    } // fin método eliminar estudiante

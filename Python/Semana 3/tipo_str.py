b5a9a923c98f1f8b837d17b086eaa19419b24a5b
Miriam <111610111+MiriamTorres30@users.noreply.github.com>
2023-10-06 00:21:52 -0300

    Tipo String: profundizando y concatenando en Python

---
+++ Python/Semana 3/tipo_str.py
@@ -0,0 +1,14 @@
+import math
+# Profundizando en el tipo String
+# Concatenación automática en Python
+
+variable = ' Adios'
+mensaje = 'Hola'' Alumnos' + variable
+mensaje += ', Terminamos'
+# print(mensaje)
+
+
+# Usamos la clase help para ayuda o documentación (built-in)
+help(str.capitalize)
+help(math)
+help(math.isnan)

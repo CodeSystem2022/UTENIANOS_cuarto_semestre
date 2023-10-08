864cbf0e974516c094abe7fd3150771fc9be8000
Carolina Belén Delgado <98441984+programacioncarolina1980@users.noreply.github.com>
2023-10-08 20:53:58 -0300

    Update and rename diferentes instalaciones que necesitaremos to diferentes instalaciones que necesitaremos.js
    se agrega  actividades de descarga  de primera semana en node js

---
+++ Node/Semana 1/diferentes instalaciones que necesitaremos.js
@@ -0,0 +1,24 @@
+// Se descarga Chrome como navegador predeterminado.//
+// Se descarga VSCode// 
+// Se descarga Postman//
+// Descargar MongoDB //
+// Descargar git //
+// se configura //
+// gitconfig--global user,name"Carolina Delgado"//
+// gitconfig--global user.email"user.email" carodelgadobelen@gmail.com //
+// Se crea cuenta en git hub //
+// Descargar nodejs.org/es //
+// Instalar Activitus Bar (extensiones de VSC) //
+// Instalar Ver.Apariencia Mostrar barras de actividades //
+// Monokai Night Theme (Extension de VSC) //
+// Type Script Importer//
+// ¿Qué es node? Es un lenguaje Backend.Acceso al sistema de archivos del equipo.//
+//Infirmación del sistema operativo. Procesos del equipo.// 
+// Node y V8//
+//Node corre sobre el motor V8 de google.//
+ // V8 es un engine de javascript  de altop desempeño, está escrito en C++,es el mismo 
+  que usa google Chrome y trabaja traduciendoel código que escribimos de jAVASCRIPT al lenguaje de la máquina, 
+  éste es tan eficiente que por eso es sorprendentemente rápido.//
+    
+
+

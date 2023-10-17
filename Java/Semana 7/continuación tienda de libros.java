b900b5ba551687ddf06d1234c5a9f496bab4658b
Celina Leonarda Costilla <88327832+Hiddencelc@users.noreply.github.com>
2023-10-16 21:34:12 -0300

    Update continuación tienda de libros.java
    De la tienda de libros, seguimos trabajando en la capa repository, service y agregamos swing

--- Java/Semana 7/continuación tienda de libros.java
+++ Java/Semana 7/continuación tienda de libros.java
@@ -1,1 +1,82 @@
+package Utn.tienda_libros.repositorio;
 
+import Utn.tienda_libros.modelo.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LibroRepositorio extends JpaRepository <Libro, Integer>{
+}
+
+
+package Utn.tienda_libros.servicio;
+
+import Utn.tienda_libros.modelo.Libro;
+
+import java.util.List;
+
+public interface ILibroServicio {
+
+    public List<Libro> listarLibros();
+    public Libro buscarLibroPorId(Integer idLibro);
+    public void guardarLibro(Libro Libro);
+    public void eliminarLibro(Libro libro);
+
+}
+
+
+package Utn.tienda_libros.servicio;
+
+
+import Utn.tienda_libros.modelo.Libro;
+import Utn.tienda_libros.repositorio.LibroRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LibroServicio implements ILibroServicio {
+    @Autowired//agrega de forma automatica una instancia
+    private LibroRepositorio libroRepositorio;
+
+    @Override
+    public List<Libro> listarLibros() {
+        return libroRepositorio.findAll();
+    }
+
+    @Override
+    public Libro buscarLibroPorId(Integer idLibro) {
+        Libro libro= libroRepositorio.findById(idLibro).orElse(null);
+        return libro;
+    }
+
+    @Override
+    public void guardarLibro(Libro libro) {
+        libroRepositorio.save(libro);
+    }
+
+    @Override
+    public void eliminarLibro(Libro libro) {
+     libroRepositorio.delete(libro);//se pasa todo el objeto para eliminar
+    }
+}
+
+public class LibroFrom extends JFrame {
+
+    LibroServicio libroServicio;
+    private JPanel panel;
+
+    @Autowired
+    public LibroFrom(LibroServicio libroServicio){
+        this.libroServicio= libroServicio;
+        iniciarForma();
+    }
+
+    private void iniciarForma(){
+         setContentPane(panel);
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//cerrar la conexion
+         setVisible(true);
+         setSize(900,700);
+    }
+
+
+}

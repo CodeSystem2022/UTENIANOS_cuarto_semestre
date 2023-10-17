ccf241dedb49e66a81a4b1985482731c5db6c1e7
Carolina Belén Delgado <98441984+programacioncarolina1980@users.noreply.github.com>
2023-10-16 21:52:03 -0300

    se agrega botón de compras
    se agrega en archivo js botón de compras

---
+++ JavaScript/Semana 2/index.js
@@ -0,0 +1,25 @@
+const shopContent = document.getElementById("shopContent");
+
+productos.forEach((product) =>{
+    const content= document.createElement("div");
+    content.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.productName}</h3>
+    <p>${product.price} $</p>
+    `;
+    shopContent.append(content);
+    const buyButton = document.createElement("Button"); //se crea botón de compras en semana 2//
+    buyButton.innerText = "Comprar";
+
+    content.append(buyButton);
+    buyButton.addEventListener("click", () =>{
+        cart.push({
+            id : product.id,
+            productName:product.productName,
+            price:product.price,
+            quanty: product.quanty,
+            img: product.img,
+        })
+    console.log(cart)
+    })
+});

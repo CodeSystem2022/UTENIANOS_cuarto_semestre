07d50e86e95ac8992937737c95214183183b080b
Carolina Belén Delgado <98441984+programacioncarolina1980@users.noreply.github.com>
2023-10-17 01:52:34 -0300

    Update 02 E-commerce Básico Parte 02.js

--- JavaScript/Semana 2/02 E-commerce Básico Parte 02.js
+++ JavaScript/Semana 2/02 E-commerce Básico Parte 02.js
@@ -1,5 +1,9 @@
-Actividades de semana 2
+// Actividades de semana 2 //
 // Se agrega emoji de carrito de compras en html//
 // Se agrega en hoja de estilo el diseño del botón (color, letra etc...)en archivo .css//
 // Se crea un carrito que es  un array vacío en archivo index.js //
 // Se crea botón comprar en index.js//
+// código carrito en js //
+// se realizan pruebas con console.log en js // 
+// se crea archivo cart js //
+// se crean contenedores en html que direccionan a archivo cart js// 

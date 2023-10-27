fcce82f5aa200cd94ded4c32b954fb19cbce0a4d
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-10-27 19:43:28 -0300

    Update callbackhell.js

--- Node/Semana 9/callbackhell.js
+++ Node/Semana 9/callbackhell.js
@@ -1,1 +1,34 @@
+function hola(nombre, miCallback){
+    setTimeout(function(){
+        console.log('Hola, '+ nombre);
+        miCallback(nombre);
+    }, 1000);
+}
 
+function hablar(callbackHablar){
+    setTimeout(function(){
+        console.log('Bla bla bla bla...');
+        callbackHablar();
+    }, 1000);
+}
+
+function adios(nombre, otroCallback){
+    setTimeout(function(){
+        console.log('Adios, '+ nombre);
+        otroCallback();
+    }, 1000);
+}
+
+// --Proceso principal
+console.log('Iniciando proceso...');
+hola('Carlos', function(nombre){
+    hablar (function(){
+        hablar(function(){
+            hablar(function(){
+                adios(nombre, function(){
+                    console.log('Terminando proceso...');
+                });
+            });
+        });
+    });
+});

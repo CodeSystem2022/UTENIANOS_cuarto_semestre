5de8fe7b054593b1936ba287fa3cec869ffbf893
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-10-27 16:02:23 -0300

    Update callback.js

--- Node/semana 8/async/callback.js
+++ Node/semana 8/async/callback.js
@@ -1,1 +1,10 @@
+function soyAsincrona() {
+  console.log("Hola, soy una función asíncrona");
+  console.log("Iniciando el proceso.");
+
+  // Llamamos a la función `soyAsincrona()` de forma asíncrona
+  setTimeout(soyAsincrona, 1000);
+
+  console.log("Terminando el proceso...");
+}
 

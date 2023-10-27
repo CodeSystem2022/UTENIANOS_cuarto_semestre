652bdcf33a0771631734366880503ca862cb9c16
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-10-27 19:48:31 -0300

    Create Como solucionar un callback hell parte 1, 2 y 3.js

---
+++ Node/Semana 9/Como solucionar un callback hell parte 1, 2 y 3.js
@@ -0,0 +1,39 @@
+function hola(nombre, miCallback){
+    setTimeout(function(){
+        console.log('Hola, '+ nombre);
+        miCallback(nombre);
+    }, 1000);
+}
+
+function hablar(callbackHablar){
+    setTimeout(function(){
+        console.log('Bla bla bla bla...');
+        callbackHablar();
+    }, 1000);
+}
+
+function adios(nombre, otroCallback){
+    setTimeout(function(){
+        console.log('Adios, '+ nombre);
+        otroCallback();
+    }, 1000);
+}
+
+function conversacion(nombre, veces, callback){
+    if(veces > 0){
+        hablar(function(){
+            conversacion(nombre, --veces, callback);
+        })
+    } else {
+        adios(nombre, callback);
+    }
+}
+
+
+// --Proceso principal
+console.log('Iniciando proceso...');
+hola('Ariel', function(nombre){
+    conversacion(nombre, 4, function(){
+        console.log('Proceso terminado');
+    });
+});

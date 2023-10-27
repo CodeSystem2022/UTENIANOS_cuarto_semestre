f95ad503ee33a60e886ea525513069db3f7a6918
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-10-27 15:35:28 -0300

    setTimeOut y pruebas
    setTimeOut y hacemos pruebas en ejecución to setTimeOut y hacemos pruebas en ejecución

---
+++ Node/Semana 3/setTimeOut y hacemos pruebas en ejecución.js
@@ -0,0 +1,18 @@
+console.log('Inicio del programa');
+
+setTimeout(function() {
+    console.log('Primer timeout');
+}, 1000);
+
+setTimeout(function() {
+    console.log('Segundo timeout');
+}, 2000);
+
+setTimeout(function() {
+    console.log('Tercer timeout');
+}, 3000);
+
+setTimeout(function() {
+    console.log('Fin del programa');
+}, 4000);
+

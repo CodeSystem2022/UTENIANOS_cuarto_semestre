2435d12695d57a7039b42a55666bedd7fcabebc9
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-10-27 19:55:48 -0300

    Update promises.js

--- Node/Semana 10/async/promises.js
+++ Node/Semana 10/async/promises.js
@@ -1,1 +1,28 @@
+function hola(nombre){
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            console.log('Hola, '+ nombre);
+            resolve(nombre);
+        }, 1000);
+    });
+}
 
+function hablar(callbackHablar){
+    setTimeout(function(){
+        console.log('Bla bla bla bla...');
+        callbackHablar();
+    }, 1000);
+}
+
+function adios(nombre, otroCallback){
+    setTimeout(function(){
+        console.log('Adios, '+ nombre);
+        otroCallback();
+    }, 1000);
+}
+
+console.log('Iniciando proceso...');
+hola('Carlos')
+    .then((nombre) => {
+        console.log('Terminando proceso...');
+    ))

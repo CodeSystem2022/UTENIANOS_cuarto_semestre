07ffe9aa32d7eb2b0d129eb4960c6a36bb4b9acb
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-10-27 16:02:52 -0300

    Update callback.js

--- Node/semana 8/async/callback.js
+++ Node/semana 8/async/callback.js
@@ -1,10 +1,18 @@
 function soyAsincrona() {
-  console.log("Hola, soy una función asíncrona");
-  console.log("Iniciando el proceso.");
+  // Declaramos una variable para el callback
+  let miCallback;
 
-  // Llamamos a la función `soyAsincrona()` de forma asíncrona
-  setTimeout(soyAsincrona, 1000);
+  // Llamamos a la función `setTimeout()`
+  setTimeout(() => {
+    // Definimos el callback
+    miCallback = () => {
+      console.log("Hola, soy una función asíncrona");
+      console.log("Iniciando el proceso...");
+    };
+
+    // Llamamos a la función `soyAsincrona()` con el callback
+    soyAsincrona(miCallback);
+  }, 1000);
 
   console.log("Terminando el proceso...");
 }
-

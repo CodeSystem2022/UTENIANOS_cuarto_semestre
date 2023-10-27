63ca42a37acc7ca06397dda85ad2ce96c024d81b
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-10-27 15:32:53 -0300

    Create 3.1-Función-flecha-con-template-String.js
    Función-flecha-con-template-String.js

---
+++ Node/Semana 3/3.1-Función-flecha-con-template-String.js
@@ -0,0 +1,5 @@
+const saludar = (nombre) =>{ //Función de flecha
+     return `Saludos de $ {nombre}`; //Usamos back tic's
+ } //esto es igual a: 'Saludos' + nombre;
+ console. log(saludar ('José'));
+                                  I

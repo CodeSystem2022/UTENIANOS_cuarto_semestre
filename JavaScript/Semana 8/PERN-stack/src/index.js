35c7d154acaca1d8142b454ce592f8824a3fa65f
Juan Agustin Saba <jsaba543@gmail.com>
2023-11-13 13:39:16 -0300

    Add files via upload
    Se cargan los archivos de la semana 8

---
+++ JavaScript/Semana 8/PERN-stack/src/index.js
@@ -0,0 +1,11 @@
+ import app from "./app.js";
+ import (pool) from "../db.js";
+
+ pool.query("SELECT NEW()", (err,res) => {
+    console.log(err,res.rows);
+    app.listen(3000);
+    console.log("Server on port", 3000);
+    pool.end();
+});   
+
+
